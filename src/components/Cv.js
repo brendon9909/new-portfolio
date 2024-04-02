fd9597cf30239097fbf822559951936b54232875
Brendon990 <stvn9909@gmail.com>
2024-04-02 17:18:54 +0200

    fixed cv page

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,7 +1,7 @@
-export default function Cv(){
-    return(
-        <div className="cv">
-            CV page
-        </div>
-    )
+export default function Cv() {
+  return (
+    <div className="cv">
+      <a target="blank" href="https://docs.google.com/document/d/1e0ZXr91sbiV4O-SOOuN9VMnFzQnCz9MM/edit?usp=sharing&ouid=103116034933565265353&rtpof=true&sd=true"><button className="cvButton">View CV</button></a>
+    </div>
+  );
 }
