bf9c91e592fa57f0d3471b461f9994e9cc5b8556
Brendon990 <stvn9909@gmail.com>
2024-04-03 15:07:15 +0200

    fixed projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,46 @@
 export default function Projects(){
     return(
         <div className="projects">
-            Projects Page
+            <div className="container">
+                <div className="proj">
+                    <span style={{position: "relative", top: "20%", left: "10%"}}>
+                    <span style={{color: "black", fontSize: "larger"}}>Project 1</span><br/>
+                    Discover your name's heritage. API-powered nationality guesses.<br/><br/>
+                    Github Repo: <a style={{textDecoration: "none", color: "inherit"}} target="blank" href="https://github.com/brendon9909/nations.git">here</a>
+                    </span>
+                </div>
+                <div className="proj">
+                <span style={{position: "relative", top: "20%", left: "10%"}}>
+                    <span style={{color: "black", fontSize: "larger"}}>Project 2</span><br/>
+                    Step into a world of fantasy and wonder at my fictional online store. Discover an array of captivating treasures waiting just for you.<br/><br/>
+                    Github Repo: <a style={{textDecoration: "none", color: "inherit"}} target="blank" href="https://github.com/brendon9909/online-store.git">here</a>
+                    </span>
+                </div>
+                <div className="proj">
+                <span style={{position: "relative", top: "20%", left: "10%"}}>
+                    <span style={{color: "black", fontSize: "larger"}}>Project 3</span><br/>
+                    Coming Soon...
+                    </span>
+                </div>
+                <div className="proj">
+                <span style={{position: "relative", top: "20%", left: "10%"}}>
+                    <span style={{color: "black", fontSize: "larger"}}>Project 4</span><br/>
+                    Coming Soon...
+                    </span>
+                </div>
+                <div className="proj">
+                <span style={{position: "relative", top: "20%", left: "10%"}}>
+                    <span style={{color: "black", fontSize: "larger"}}>Project 5</span><br/>
+                    Coming Soon...
+                    </span>
+                </div>
+                <div className="proj">
+                <span style={{position: "relative", top: "20%", left: "10%"}}>
+                    <span style={{color: "black", fontSize: "larger"}}>Project 6</span><br/>
+                    Coming Soon...
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
